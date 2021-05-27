cef3b72b599866f6a0f5e73d75787ee27ca0c7c5
hirrolot <hirrolot@gmail.com>
2021-05-27 19:33:12 +0600

    Simplify `examples/state.c`

--- examples/state.c
+++ examples/state.c
@@ -22,10 +22,15 @@ void MyState_set(void *self, int x) {
 
 impl(State, MyState);
 
+/*
+ * Output:
+ * x = 0
+ * x = 5
+ */
 int main(void) {
     MyState state = {0};
 
-    printf("Initial state = %d.\n", VTABLE(State, MyState).get(&state));
+    printf("x = %d\n", VTABLE(State, MyState).get(&state));
     VTABLE(State, MyState).set(&state, 5);
-    printf("Updated state = %d.\n", VTABLE(State, MyState).get(&state));
+    printf("x = %d\n", VTABLE(State, MyState).get(&state));
 }
